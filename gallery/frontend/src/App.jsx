8b1e801730f4ae3929d42797b55a49cd322ebff7
Leon <leon.moelker@gmail.com>
2025-11-22 14:50:40 +0000

    Add HomePage

--- gallery/frontend/src/App.jsx
+++ gallery/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import ShowcaseAdd from "./components/ShowcaseAdd.jsx";
 import Login from "./components/Login.jsx";
@@ -9,7 +10,8 @@ const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/" element={<Home />} />
+                <Route path='/showcase' element={<Dashboard />} />
                 <Route path="/showcase/:id" element={<Dashboard />} />
                 <Route path="/showcase/add" element={<ShowcaseAdd />} />
                 <Route path="/login" element={<Login />} />
---
+++ gallery/frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "./Navbar.jsx";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+
+        </>
+    );
+};
+
+export default Home;
