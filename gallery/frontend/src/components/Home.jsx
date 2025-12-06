57eda86514243425666299d2a4e336f995d04283
Leon <leon.moelker@gmail.com>
2025-12-06 14:08:31 +0000

    carousel

--- gallery/frontend/src/components/Home.jsx
+++ gallery/frontend/src/components/Home.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from "./Navbar.jsx";
+import {
+    Carousel,
+    CarouselSlider,
+    CarouselCard,
+    CarouselNavContainer,
+    CarouselNav,
+    CarouselNavButton
+} from "@fluentui/react-carousel"
+import ShowcaseService from "../service/ShowcaseService.jsx";
+import {Button} from "@fluentui/react-components";
+import {Link, useNavigate} from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+    const [showcases, setShowcases] = useState([]);
+
+    useEffect(() => {
+        ShowcaseService.getAll()
+            .then(response => {
+                setShowcases(response.data);
+            })
+    })
+
     return (
         <>
             <Navbar />
+            <Carousel groupSize={1} circular className="h-[80vh] relative">
+                <CarouselSlider className="h-full">
+                    {showcases.map((showcase, index) => (
+                        <CarouselCard key={index} index={index} className="relative h-full w-full">
+                            <img
+                                src={`http://localhost:8080/assets/images/showcases/${showcase.heroImage}`}
+                                alt={showcase.title}
+                                className="w-full h-full object-cover object-center"
+                            />
+                            <div className="absolute inset-0 bg-black/20 z-0"></div>
+                            <div className="absolute left-1/2 bottom-1/8 -translate-x-1/2 z-10 flex flex-col items-center text-center">
+                                <h1 className="!text-white">
+                                    {showcase.title}
+                                </h1>
+                                <Link to={`/showcase/${showcase.id}`}>
+                                    <Button
+                                        appearance="primary"
+                                        style={{backgroundColor: '#6574A2', color: 'white'}}
+                                    >
+                                        Browse this showcase
+                                    </Button>
+                                </Link>
+                            </div>
+                        </CarouselCard>
+                    ))}
+                </CarouselSlider>
 
+                <CarouselNavContainer
+                    className="absolute bottom-4 w-full z-20"
+                    prev={{
+                        "aria-label": "Previous slide",
+                        className: "!bg-white/50 hover:!bg-white"
+                    }}
+                    next={{
+                        "aria-label": "Next slide",
+                        className: "!bg-white/50 hover:!bg-white"
+                    }}
+                >
+                    <CarouselNav>
+                        {(_, index) => (
+                            <CarouselNavButton aria-label={`Go to slide ${index + 1}`} />
+                        )}
+                    </CarouselNav>
+                </CarouselNavContainer>
+            </Carousel>
         </>
     );
 };
