f4ffec475ab3e442e201b556615e77a8f7714ad3
Leon <leon.moelker@gmail.com>
2025-12-07 11:27:52 +0000

    Homepage Update 2

---
+++ gallery/frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="bg-[#6574A2] h-[50vh] px-32 py-16 !text-white">
+            <p>Footer</p>
+        </div>
+    );
+};
+
+export default Footer;
